8c1515653ba98a104697ecb35a02e4a9edc21ae2
Ido Ben-Natan <idobn@Idos-MBP.lan>
2016-07-23 23:38:00 +0300

    Token Types

--- csrc/lexer.h
+++ csrc/lexer.h
@@ -2,17 +2,7 @@
 #define _LEXER_H
 
 #include <vector>
-#include <string>
-
-enum TokenType {
-	UNKNOWN = 0,
-	WHITESPACE = 1
-};
-
-typedef struct {
-	TokenType type;
-	std::string lexeme;
-} Token;
+#include "token.h"
 
 void tokenize(std::string, std::vector<Token>&);
 
